997dee57a810b79b504a886233c968d68140231b
wimmeldj <davidwimmel@gmail.com>
2022-07-24 15:28:10 -0400

    gen-tags update

--- gen-tags.sh
+++ gen-tags.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 # -*- explicit-shell-file-name: /bin/bash; -*-
 
+#
+# ctags --list-kinds-full c | awk '$1 == "C" { print; } NR == 1 { print; }' run
+#
+# above to get "kinds" of tags. Exclude or disclude a tag kind with
+# --kinds-C=+{addKind} or --kinds-C=-{remKind}
+#
+# e.g. --kinds-C=+{label}{prototype} tells c-tags to generate tags for labels
+# and prototypes which isn't done by default
+#
+# 
+# A not uncommon phenomenon is for macros placed before relevant symbol
+# declares/defns to screw up the ctags parser. To remedy this, you can instruct
+# ctags to ignore the macro usage.
+#
+# -I IDENTIFIER  - to ignore the identifier
+# -I IDENTIFIER+ - to ignore the identifier and any paren enclosed args
+#
+# e.g.
+# https://docs.microsoft.com/en-us/windows-hardware/drivers/devtest/irql-annotations-for-drivers
+#
+# -I _IRQL_requires_same_ to ignore : _IRQL_requires_same_ fun(...);
+# -I _IRQL_requires_max_+  to ignore: _IRQL_requires_max_(irql) fun(...);
+#
+# See Man:ctags(1) for syntax to provide this list as a file if you'd rather
+#
+
 run() {
     # key : val
     # dir : extra args
@@ -24,7 +50,13 @@ run() {
     # targs["$PWD/minkernel/ntos/whea"]=
     # targs["$PWD/minkernel/ntos/inc"]=
     # targs["$PWD/minkernel/published/internal"]="--language-force=C"
-    targs[]
+    targs["$PWD/urbit"]=
+    targs["$PWD/urcrypt"]=
+    targs["$PWD/ent"]=
+    targs["$PWD/hs"]=
+    # parsing the whole /usr/include causes lookup to take > 1 sec.
+    targs["/usr/include/uv.h"]="--language-force=C"
+    targs["/usr/include/uv"]=
 
     local dir args
     for dir in "${!targs[@]}"; do
@@ -40,22 +72,18 @@ run() {
         echo "tagging $dir" >&2
 
         local cmd="
-ctags-exuberant -o $tagfile
+ctags -o $tagfile
 -eR
--I _IRQL_requires_max_+
--I __drv_allocatesMem+
--I _IRQL_requires_min_+
--I _IRQL_requires_same_
--I _Must_inspect_result_
+--kinds-C=+{label}{prototype}
 $args
 $dir
 "
         eval $cmd
-        
+
     done
 
     # finally, create a single unified TAGS file with references to all
-    ctags-exuberant -e -o TAGS "${tags[@]}" .
+    ctags -e -o TAGS "${tags[@]}" .
 }
 
 
